257573df06029afa34e0f8edf25b731f8ee65e63
VitorMeneses <vitorramiloks@gmail.com>
2018-08-14 17:20:58 -0300

    Commit Initial

---
+++ adivinhacao.py
@@ -0,0 +1,16 @@
+print("*********************************")
+print("Bem Vindo ao jogo de Adivinhação Numerica")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = input("Digite o seu numero: ")
+
+print("Você Digitou: ", chute)
+
+if(numero_secreto == chute):
+    print("Você acertou")
+else:
+    print("Erroooooooooooou")
+
+    
