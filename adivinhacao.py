7c7e6f0e8c983bcc03a858d50080e8eaeaa153ad
VitorMeneses <vitorramiloks@gmail.com>
2018-08-14 18:29:16 -0300

    Loop corrections

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,30 +3,30 @@ print("Bem Vindo ao jogo de Adivinhação")
 print("*********************************")
 
 numero_secreto = 42
-tentativas = 5
+total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= tentativas):
-    print("Tentativa", rodada, "de", tentativas)
-    chute_strg = input("Digite o seu numero: ")
-    print("Você Digitou: ", chute_strg)
-    chute = int (chute_strg)
+while (rodada <= total_de_tentativas):
+        print("Tentativa", rodada, "de", total_de_tentativas)
+        chute_strg = input("Digite o seu numero: ")
+        print("Você Digitou: ", chute_strg)
+        chute = int(chute_strg)
 
-    acertou = numero_secreto == chute
-    maior   = chute > numero_secreto
-    menor   = chute < numero_secreto
+        acertou = numero_secreto == chute
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+
+        if (acertou):
+            print("Você acertou")
+        else:
+            if (maior):
+                print("Você errou! O Seu chute foi maior que o numero secreto. ")
+            elif (menor):
+                print("Você errou! O seu chute foi menor do que o numero secreto. ")
+
+rodada = rodada + 1
 
 
-    if(acertou):
-        print("Você acertou")
-    else:
-        if(maior):
-            print("Você errou! O Seu chute foi maior que o numero secreto. ")
-        elif(menor):
-            print("Você errou! O seu chute foi menor do que o numero secreto. ")
 
-
-    rodada = rodada + 1
-
-    print("Final de Jogo")
+print("Final de Jogo")
 
