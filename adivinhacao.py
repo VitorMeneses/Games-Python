a9f5108f0f0f24c564f70c04a094414f792ae3dc
VitorMeneses <vitorramiloks@gmail.com>
2018-08-14 17:27:16 -0300

    Implementing string to integer conversion

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,16 +1,18 @@
 print("*********************************")
-print("Bem Vindo ao jogo de Adivinhação Numerica")
+print("Bem Vindo ao jogo de Adivinhação")
 print("*********************************")
 
 numero_secreto = 42
 
-chute = input("Digite o seu numero: ")
+chute_strg = input("Digite o seu numero: ")
 
-print("Você Digitou: ", chute)
+
+print("Você Digitou: ", chute_strg)
+
+chute = int (chute_strg)
 
 if(numero_secreto == chute):
     print("Você acertou")
 else:
     print("Erroooooooooooou")
 
-    
