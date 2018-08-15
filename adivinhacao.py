936c99e07b9cc1f1fcc4f2ccd6be7d954af00e42
VitorMeneses <vitorramiloks@gmail.com>
2018-08-15 18:15:56 -0300

    Implementing menu of choice of games

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,6 +6,18 @@ print("Bem Vindo!", "                                             ***")
 print("Descubra o número secreto", "                              ***")
 print("***********************************************************")
 
+print ("Deseja iniciar o jogo?")
+print("1 (Sim)     2(Não)")
+print("***********************************************************")
+inicio = int(input(">>> "))
+print("***********************************************************")
+
+if inicio == 1:
+    print("Birrrrllll, ta sainda da jaula o monstro")
+    print()
+elif inicio == 2:
+    exit("Arregooooooooooou")
+
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 0
 pontos = 1000
@@ -17,22 +29,19 @@ print("***********************************************************")
 
 nivel = int(input("Defina o nivel: "))
 
-
 while(nivel > 3):
-    nivel = int(input("Informe a Dificuldade: (1) Fácil (2) Médio (3) Difícil: "))
+    nivel = int(input("Informe a Dificuldade: (1) Fácil (2) Médio (3) Difícil:    "))
     if (nivel):
         print("***********************************************************")
         continue
 
-if nivel == None:
-    total_de_tentativas = 1
+if (nivel == 1):
+    total_de_tentativas = 20
 else:
-    if(nivel == 1):
-        total_de_tentativas = 20
-    if (nivel == 2):
-        total_de_tentativas = 10
-    elif(nivel == 3):
-        total_de_tentativas = 5
+     if (nivel == 2):
+         total_de_tentativas = 10
+     elif(nivel == 3):
+         total_de_tentativas = 5
 
 for rodada in range (1, total_de_tentativas + 1):
         print("***********************************************************")
@@ -40,6 +49,11 @@ for rodada in range (1, total_de_tentativas + 1):
         chute_strg = input("Digite o seu número entre 1 a 100: ")
         print("***********************************************************")
         print("Você Digitou: ", chute_strg)
+        if not chute_strg.isdigit():
+            print("Você deveria digitar um valor númerico....")
+            continue
+
+
         chute = int(chute_strg)
 
         if (chute < 1 or chute > 100):
