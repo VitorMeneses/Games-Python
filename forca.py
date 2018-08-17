332e96ef41cfaa4e71ceb57bb6809261aba2a393
VitorMeneses <vitorramiloks@gmail.com>
2018-08-17 19:50:03 -0300

    While-finished implementation

--- forca.py
+++ forca.py
@@ -17,50 +17,43 @@ def jogar_forca():
     elif inicio == 2:
         exit("Até mais")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    acertos = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(acertos)
 
     while(not enforcou and not acertou):
+
         chute = input("Qual a letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    acertos[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in acertos
+        print(acertos)
 
 
-        print("jogando....")
+    if(acertou):
+        print("Você Ganhou!!!")
+    else:
+        print("Você perdeu!! :(")
 
-
-
-
-    print("Fim de Jogo")
 
 if(__name__ == "__main__"):
     jogar_forca()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 print("***********************************************************")
 print("Final de Jogo")
