dcbe0a8beed669a27e40333f20c57ddbea122834
VitorMeneses <vitorramiloks@gmail.com>
2018-08-20 20:02:18 -0300

    language change

--- Games.py
+++ Games.py
@@ -10,11 +10,11 @@ print ("Choose your game")
 print("(1) Gallows     (2) Divination")
 print("***********************************************************")
 
-jogo = int(input(">>>"))
+game = int(input(">>>"))
 
-if (jogo == 1):
+if (game == 1):
     print("Starting Gallows")
     gallows.play_gallows()
-elif (jogo == 2):
+elif (game == 2):
     print("Starting Divination")
     divination.play_divination()
