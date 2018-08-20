7b4ecebdebb0d8b36b62174182e5f2e6c98a9f95
VitorMeneses <vitorramiloks@gmail.com>
2018-08-20 19:56:49 -0300

    Changing the language of the code from Portuguese to English, leaving the universal code

--- Games.py
+++ Games.py
@@ -1,21 +1,20 @@
-import adivinhacao
-import forca
+import divination
+import gallows
 
 print("***********************************************************")
 print("***********************************************************")
-print("******************Escolha seu Jogo*************************")
 print("***********************************************************")
 
 
-print ("Escolha seu Jogo")
-print("(1) Forca     (2) Adivinhação")
+print ("Choose your game")
+print("(1) Gallows     (2) Divination")
 print("***********************************************************")
 
 jogo = int(input(">>>"))
 
 if (jogo == 1):
-    print("Iniciando Forca")
-    forca.jogar_forca()
+    print("Starting Gallows")
+    gallows.play_gallows()
 elif (jogo == 2):
-    print("iniciando Adivinhação")
-    adivinhacao.play_divination()
+    print("Starting Divination")
+    divination.play_divination()
---
+++ gallows.py
@@ -0,0 +1,82 @@
+import random
+import illustration
+
+def play_gallows():
+
+    print("***********************************************************")
+    print("***********************************************************")
+    print("Welcome to the gallows game.!", "                            ***")
+    print("***********************************************************")
+
+    print ("Do you want to start the game?")
+    print("1 (Yes)     2 (No)")
+    print("***********************************************************")
+    start = int(input(">>> "))
+    print("***********************************************************")
+
+    if start == 1:
+        print("Loading [...]")
+        print()
+    elif start == 2:
+        exit("See You")
+
+
+    archive = open("words.txt", "r", encoding="utf8")
+    words = []
+
+    for line in archive:
+        line = line.strip()
+        words.append(line)
+
+
+    archive.close()
+
+    number = random.randrange(0, len(words))
+    secret_words = words[number].upper()
+
+
+    hits = ["_" for lyrics in secret_words]
+
+
+    hanged = False
+    hit = False
+    error = 0
+
+    print(hits)
+    print("The word has {} letters".format(len(secret_words)))
+
+    while(not hanged and not hit):
+
+        kick = input("What is the letter? ")
+        kick = kick.strip().upper()
+
+        if(kick in secret_words):
+            index = 0
+            for lyrics in secret_words:
+                if(kick == lyrics):
+                    hits[index] = lyrics
+                index += 1
+        else:
+            error += 1
+            print("Oops, you screwed up! Missing {} attempts.".format(7-error))
+            illustration.illustration_gallows(error)
+
+
+        hanged = error == 7
+        hit = "_" not in hits
+        print(hits)
+
+
+    if(hit):
+        illustration.illustration_trophy()
+
+    else:
+        illustration.illustration_skull()
+        print("You lost!! :( and the word was {}".format(secret_words))
+
+if(__name__ == "__main__"):
+    play_gallows()
+
+print("***********************************************************")
+print("End")
+print("***********************************************************")
